61ff0cd5ece1fe83c7ba690f3711084ceafc00d4
younghyunHan <atuniverse@naver.com>
2022-12-30 10:50:35 +0900

    Add: FullscreenControl, GeolocateControl, NavigationControl 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import logo from "./logo.svg";
+import React, { useRef, useState, useEffect } from "react";
+// import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import Map, {
+  FullscreenControl,
+  GeolocateControl,
+  NavigationControl,
+} from "react-map-gl";
+
 import "./App.css";
 
 function App() {
-  return <div className="App">1</div>;
+  const MY_ACCESS_TOKEN =
+    "pk.eyJ1IjoieW91bmdoeXVuaGFuIiwiYSI6ImNsYzdqNjB1ODE5Z2szcGw4d2Z5NjQyMWUifQ.WaDzYMTuTv6UROCimI0H2g";
+
+  return (
+    <div className="App">
+      <Map
+        initialViewState={{
+          longitude: -100,
+          latitude: 40,
+          zoom: 3.5,
+        }}
+        style={{ width: "100vw", height: "100vh" }}
+        mapStyle="mapbox://styles/mapbox/dark-v11"
+        mapboxAccessToken={MY_ACCESS_TOKEN}
+      >
+        <FullscreenControl />
+        <GeolocateControl />
+        <NavigationControl />
+      </Map>
+    </div>
+  );
 }
 
 export default App;
